b4ad86bf6a00d424e7d7598420adc10bc90a243a
cochranpf <cochranpf@vcu.edu>
2021-05-02 23:58:21 -0400

    Recoded Everything

--- RaceTrack.java
+++ RaceTrack.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -10,6 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RaceTrack extends Application{
     public static void main(String[] args){
@@ -26,23 +28,7 @@ public class RaceTrack extends Application{
 
 
 
-        EventHandler<ActionEvent> startPress = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e){
-                System.out.println("Start Pressed.");
-            }
-        };
-
-        EventHandler<ActionEvent> pausePress = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e){
-                System.out.println("Pause Pressed.");
-            }
-        };
-
-        EventHandler<ActionEvent> resetPress = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e){
-                System.out.println("Reset Pressed.");
-            }
-        };
+        
 
         VBox root = new VBox();
         Scene startScene = new Scene(root);
@@ -52,10 +38,6 @@ public class RaceTrack extends Application{
         Button startButton = new Button("Start");
         Button pauseButton = new Button("Pause");
         Button resetButton = new Button("Reset");
-
-        startButton.setOnAction(startPress);
-        pauseButton.setOnAction(pausePress);
-        resetButton.setOnAction(resetPress);
 
         buttonZone.getChildren().addAll(startButton, pauseButton, resetButton);
 
@@ -82,7 +64,38 @@ public class RaceTrack extends Application{
         track2.setFill(Color.DARKGREY);
         track3.setFill(Color.DARKGREY);
 
-        
+
+        //SET UP CARS
+        Car Car1 = new Car(1, car1);
+        Car Car2 = new Car(2, car2);
+        Car Car3 = new Car(3, car3);
+
+
+        //EVENT HANDLERS
+
+        EventHandler<ActionEvent> startPress = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e){
+                
+            }
+        };
+
+        EventHandler<ActionEvent> pausePress = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e){
+                System.out.println("Pause Pressed.");
+            }
+        };
+
+        EventHandler<ActionEvent> resetPress = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e){
+                System.out.println("Reset Pressed.");
+            }
+        };
+
+        startButton.setOnAction(startPress);
+        pauseButton.setOnAction(pausePress);
+        resetButton.setOnAction(resetPress);
+
+        //START GUI
 
         root.getChildren().addAll(buttonZone, car1, track1, car2, track2, car3, track3);
 
@@ -90,5 +103,35 @@ public class RaceTrack extends Application{
         primaryStage.setScene(startScene);
         primaryStage.show();
         
+    }
+}
+
+class Car extends Thread{
+    private int ID;
+    private int distance;
+    private ImageView carImage;
+
+    public Car(int id, ImageView aCar){
+        this.ID = id;
+        this.distance = 0;
+        this.carImage = aCar;
+    }
+
+    @Override
+    public void run(){
+        while(true) {
+            Platform.runLater(()->{
+                int increase = 0;
+                increase = ThreadLocalRandom.current().nextInt(0, 11);
+                this.carImage.setTranslateX(increase);
+                this.distance = this.distance + increase;
+                if (this.distance >= 500){
+                    System.out.println("Car #" + this.ID + " wins!");
+                }
+                else{
+                    System.out.println("Car #" + this.ID + " moved " + increase + " pixels.");
+                }
+            });
+        }
     }
 }
